52879b68dddcaac995bc9bdf5064d8e864a48166
Thiago Rosendo <thiago_953@hotmail.com>
2025-09-30 18:40:14 +0000

    0. Criar uma aplicação CRUD para cadastro de Clientes e Produtos usando Hibernate + JPA + SQLite

---
+++ jpa_hibernate/administrativo-api/src/main/java/com/techcamps/cadastros/entities/Pessoa.java
@@ -0,0 +1,62 @@
+package com.techcamps.cadastros.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Pessoa {
+
+    @Id
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+    private String cpf;
+    private Date dtNascimento;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Date getDtNascimento() {
+        return dtNascimento;
+    }
+
+    public void setDtNascimento(Date dtNascimento) {
+        this.dtNascimento = dtNascimento;
+    }
+}
